d3fac93965f9e6d24b8f24f01634430986d86c9a
saptarshi <saptarshi.sinha@bristol.ac.uk>
2023-11-09 16:04:29 +0000

    added timestamp sampling to repcount

--- exemplar_counting_train.py
+++ exemplar_counting_train.py
@@ -85,7 +85,7 @@ def get_args_parser():
                         help='path where to tensorboard log')
     parser.add_argument("--title", default="CounTR_finetuning", type=str)
     parser.add_argument("--wandb", default="counting", type=str)
-    parser.add_argument("--team", default="long-term-video", type=str)
+    parser.add_argument("--team", default="repetition_counting", type=str)
     parser.add_argument("--wandb_id", default=None, type=str)
 
     return parser
